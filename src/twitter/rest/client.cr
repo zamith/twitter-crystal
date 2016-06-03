bbd24db1df267ba0a38d493f4a3f893a6f909804
Zamith <zamith.28@gmail.com>
2016-06-04 00:45:51 +0100

    Change the way default values are defined

--- src/twitter/rest/client.cr
+++ src/twitter/rest/client.cr
@@ -13,7 +13,7 @@ module Twitter
       Host = "api.twitter.com"
       property :access_token, :access_token_secret, :consumer_key, :consumer_secret, :user_agent
 
-      def initialize(@consumer_key : String, @consumer_secret : String, @access_token : String, @access_token_secret : String, @user_agent = nil : Nil | String)
+      def initialize(@consumer_key : String, @consumer_secret : String, @access_token : String, @access_token_secret : String, @user_agent : Nil | String = nil)
         @user_agent ||= "CrystalTwitterClient/#{Twitter::Version.to_s}"
         consumer = OAuth::Consumer.new(Host, consumer_key, consumer_secret)
         access_token = OAuth::AccessToken.new(access_token, access_token_secret)
